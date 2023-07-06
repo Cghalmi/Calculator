40bcba2a246243a19850995eea3e6e00bcf541c0
chakibGhalmi <chakibus1@yahoo.fr>
2023-07-06 10:56:54 -0400

    add test

--- Calculatrice/Program.cs
+++ Calculatrice/Program.cs
@@ -13,18 +13,27 @@ namespace Calculatrice
             var toCalcule1 = "6*(4/2)+3*1"; //15  
             var toCalcule2 = "6/3-1"; //1  
             var toCalcule3 = "((3+5)-6)"; //1  
-            var toCalcule4 = "6*(4/2)+(3*1)"; //15   
+            var toCalcule4 = "6*(4/2)+(3*1)"; //15
+            var toCalcule5 = "(((3+5)-6)*3)"; //1  
             var cal = new Calculator2();
             var result = cal.Eval(toCalcule1);
+
+             
             Console.WriteLine($"resutlt of {toCalcule1} : {result.ToString()}");
 
             var result2 = cal.Eval(toCalcule2);
             Console.WriteLine($"resutlt of {toCalcule2} : {result2.ToString()}");
-            var result4 = cal.Eval(toCalcule4);
-            Console.WriteLine($"resutlt of {toCalcule4} : {result4.ToString()}");
 
             var result3 = cal.Eval(toCalcule3);
             Console.WriteLine($"resutlt of {toCalcule3} : {result3.ToString()}");
+
+            var result4 = cal.Eval(toCalcule4);
+            Console.WriteLine($"resutlt of {toCalcule4} : {result4.ToString()}");
+
+
+            var result5 = cal.Eval(toCalcule5);
+            Console.WriteLine($"resutlt of {toCalcule5} : {result5.ToString()}");
+
         }
 
       
