7028f684df52c561ca2176318e725aa7a843741c
chakibGhalmi <chakibus1@yahoo.fr>
2023-07-06 10:55:04 -0400

    Add new calculated based on priorities

---
+++ Calculatrice/Calculator2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculatrice
+{
+    public class Calculator2
+    {
+        public string Eval(string str)
+        {
+            var dic = new Dictionary<string, int>();
+            dic.Add("(", 1);
+            dic.Add("/", 2);
+            dic.Add("*", 2);
+            dic.Add("+", 3);
+            dic.Add("-", 3);
+            char[] chars = { '*', '+', '-', '/' };
+            while (dic.Any(x => str.Contains(x.Key)))
+            {
+                int i = 0;
+                while (dic.Count > 0)
+                {
+                    var key = dic.ElementAt(i).Key;
+                    var index = str.IndexOf(key);
+                    var Resultdouble = 0.0d;
+                    var left = "";
+                    var right = "";
+
+                    var anyOperator = str.IndexOfAny(chars);
+                    if (anyOperator == -1)
+                        return str;
+
+                    if (index != -1)
+                    {
+                        switch (key)
+                        {
+                            case "(":
+                                var endPar = str.IndexOf(')', index);
+                                var endlength = endPar != -1 ? endPar - index : str.Length;
+                                var firstValue = str.Substring(index, endlength);
+                                var betwenvalue = firstValue.Substring(1, firstValue.Length - 1);
+                                var exitsIndex = betwenvalue.IndexOf("(");
+
+                                if (exitsIndex != -1)
+                                {
+                                    exitsIndex = betwenvalue.LastIndexOf("(");
+                                    endPar = betwenvalue.IndexOf(')', index);
+                                    endlength = endPar != -1 ? endPar - exitsIndex - 1 : betwenvalue.Length - 1;
+                                    betwenvalue = betwenvalue.Substring(exitsIndex + 1, endlength - exitsIndex);
+                                }
+
+                                var result = Eval(betwenvalue);
+                                str = str.Replace("(" + betwenvalue + ")", result);
+                                break;
+
+                            case "/":
+                                calculate(str, chars, index, out left, out right);
+
+                                Resultdouble = Convert.ToDouble(left) / Convert.ToDouble(right);
+                                str = str.Replace(left + "/" + right, Resultdouble.ToString());
+                                break;
+
+                            case "*":
+                                calculate(str, chars, index, out left, out right);
+
+                                Resultdouble = Convert.ToDouble(left) * Convert.ToDouble(right);
+                                str = str.Replace(left + "*" + right, Resultdouble.ToString());
+                                break;
+
+                            case "+":
+                                calculate(str, chars, index, out left, out right);
+
+                                Resultdouble = Convert.ToDouble(left) + Convert.ToDouble(right);
+                                str = str.Replace(left + "+" + right, Resultdouble.ToString());
+                                break;
+                            case "-":
+                                calculate(str, chars, index, out left, out right);
+
+                                Resultdouble = Convert.ToDouble(left) - Convert.ToDouble(right);
+                                str = str.Replace(left + "-" + right, Resultdouble.ToString());
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        dic.Remove(key);
+                    }
+                }
+            }
+
+            return str;
+        }
+
+        private static void calculate(string str, char[] chars, int index, out string left, out string right)
+        {
+            var leftIndex = str.IndexOfAny(chars, 0);
+            var leftStart = leftIndex == -1 || leftIndex == index ? 0 : leftIndex + 1;
+            left = str.Substring(leftStart, Math.Abs(index - leftStart));
+            var rightIndex = str.IndexOfAny(chars, index + 1);
+            var rightStart = rightIndex == -1 ? str.Length - index : rightIndex - index;
+            right = str.Substring(index + 1, rightStart - 1);
+        }
+    }
+}
--- Calculatrice/Program.cs
+++ Calculatrice/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Calculatrice
 {
@@ -7,11 +10,23 @@ namespace Calculatrice
 
         public static void Main(string[] args)
         {
-            var toCalcule = "(2+5)*3"; //21  
-            var calculator = new Calculator();
-            var result = calculator.CalculeWithExpFromString(toCalcule);
+            var toCalcule1 = "6*(4/2)+3*1"; //15  
+            var toCalcule2 = "6/3-1"; //1  
+            var toCalcule3 = "((3+5)-6)"; //1  
+            var toCalcule4 = "6*(4/2)+(3*1)"; //15   
+            var cal = new Calculator2();
+            var result = cal.Eval(toCalcule1);
+            Console.WriteLine($"resutlt of {toCalcule1} : {result.ToString()}");
 
-            Console.WriteLine($"resutlt of {toCalcule} : {result.ToString()}");
+            var result2 = cal.Eval(toCalcule2);
+            Console.WriteLine($"resutlt of {toCalcule2} : {result2.ToString()}");
+            var result4 = cal.Eval(toCalcule4);
+            Console.WriteLine($"resutlt of {toCalcule4} : {result4.ToString()}");
+
+            var result3 = cal.Eval(toCalcule3);
+            Console.WriteLine($"resutlt of {toCalcule3} : {result3.ToString()}");
         }
+
+      
     }
 }
